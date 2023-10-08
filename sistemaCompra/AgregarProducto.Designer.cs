0f52e6fcc6ead72d783c414833f6d41d19e21eee
schweb04 <manelequintero@gmail.com>
2023-10-08 18:58:59 -0400

    Forma Agregar Producto (pendiente a cambiar el diseño, porque se usó el base de Windows Forms)

--- sistemaCompra/AgregarProducto.Designer.cs
+++ sistemaCompra/AgregarProducto.Designer.cs
@@ -28,12 +28,232 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AgregarProducto";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AgregarProducto));
+            lblCodigo = new Label();
+            txtCodigo = new TextBox();
+            pictureBox5 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            lblNombre = new Label();
+            txtNombre = new TextBox();
+            lblCantidad = new Label();
+            txtCantidad = new TextBox();
+            lblUnidad = new Label();
+            txtUnidad = new TextBox();
+            lblCostoUnitario = new Label();
+            txtCosto = new TextBox();
+            lblAgregarProducto = new Label();
+            lblPrecio = new Label();
+            txtPrecio = new TextBox();
+            btnAceptar = new Button();
+            btnCancelar = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // lblCodigo
+            // 
+            lblCodigo.AutoSize = true;
+            lblCodigo.Location = new Point(55, 64);
+            lblCodigo.Name = "lblCodigo";
+            lblCodigo.Size = new Size(49, 15);
+            lblCodigo.TabIndex = 0;
+            lblCodigo.Text = "Código ";
+            // 
+            // txtCodigo
+            // 
+            txtCodigo.Location = new Point(110, 64);
+            txtCodigo.Name = "txtCodigo";
+            txtCodigo.Size = new Size(100, 23);
+            txtCodigo.TabIndex = 1;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.ImeMode = ImeMode.NoControl;
+            pictureBox5.Location = new Point(722, 11);
+            pictureBox5.Margin = new Padding(3, 2, 3, 2);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(24, 20);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 19;
+            pictureBox5.TabStop = false;
+            pictureBox5.Click += pictureBox5_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.ImeMode = ImeMode.NoControl;
+            pictureBox4.Location = new Point(693, 11);
+            pictureBox4.Margin = new Padding(3, 2, 3, 2);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(23, 36);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 20;
+            pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Location = new Point(55, 105);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(51, 15);
+            lblNombre.TabIndex = 21;
+            lblNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(110, 105);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(151, 23);
+            txtNombre.TabIndex = 22;
+            // 
+            // lblCantidad
+            // 
+            lblCantidad.AutoSize = true;
+            lblCantidad.Location = new Point(37, 153);
+            lblCantidad.Name = "lblCantidad";
+            lblCantidad.Size = new Size(55, 15);
+            lblCantidad.TabIndex = 23;
+            lblCantidad.Text = "Cantidad";
+            // 
+            // txtCantidad
+            // 
+            txtCantidad.Location = new Point(98, 153);
+            txtCantidad.Name = "txtCantidad";
+            txtCantidad.Size = new Size(63, 23);
+            txtCantidad.TabIndex = 24;
+            txtCantidad.TextAlign = HorizontalAlignment.Right;
+            // 
+            // lblUnidad
+            // 
+            lblUnidad.AutoSize = true;
+            lblUnidad.Location = new Point(177, 153);
+            lblUnidad.Name = "lblUnidad";
+            lblUnidad.Size = new Size(84, 15);
+            lblUnidad.TabIndex = 25;
+            lblUnidad.Text = "Ud. de Medida";
+            // 
+            // txtUnidad
+            // 
+            txtUnidad.Location = new Point(267, 153);
+            txtUnidad.Name = "txtUnidad";
+            txtUnidad.Size = new Size(84, 23);
+            txtUnidad.TabIndex = 26;
+            txtUnidad.TextAlign = HorizontalAlignment.Right;
+            // 
+            // lblCostoUnitario
+            // 
+            lblCostoUnitario.AutoSize = true;
+            lblCostoUnitario.Location = new Point(374, 153);
+            lblCostoUnitario.Name = "lblCostoUnitario";
+            lblCostoUnitario.Size = new Size(83, 15);
+            lblCostoUnitario.TabIndex = 27;
+            lblCostoUnitario.Text = "Costo Unitario";
+            // 
+            // txtCosto
+            // 
+            txtCosto.Location = new Point(463, 153);
+            txtCosto.Name = "txtCosto";
+            txtCosto.Size = new Size(69, 23);
+            txtCosto.TabIndex = 28;
+            txtCosto.TextAlign = HorizontalAlignment.Right;
+            // 
+            // lblAgregarProducto
+            // 
+            lblAgregarProducto.AutoSize = true;
+            lblAgregarProducto.Font = new Font("Gill Sans MT", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            lblAgregarProducto.ForeColor = Color.Black;
+            lblAgregarProducto.Location = new Point(275, 11);
+            lblAgregarProducto.Name = "lblAgregarProducto";
+            lblAgregarProducto.Size = new Size(248, 38);
+            lblAgregarProducto.TabIndex = 29;
+            lblAgregarProducto.Text = "Agregar Producto";
+            // 
+            // lblPrecio
+            // 
+            lblPrecio.AutoSize = true;
+            lblPrecio.Location = new Point(553, 153);
+            lblPrecio.Name = "lblPrecio";
+            lblPrecio.Size = new Size(88, 15);
+            lblPrecio.TabIndex = 30;
+            lblPrecio.Text = "Precio de Venta";
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(647, 153);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(69, 23);
+            txtPrecio.TabIndex = 31;
+            txtPrecio.TextAlign = HorizontalAlignment.Right;
+            // 
+            // btnAceptar
+            // 
+            btnAceptar.Location = new Point(667, 218);
+            btnAceptar.Name = "btnAceptar";
+            btnAceptar.Size = new Size(75, 23);
+            btnAceptar.TabIndex = 32;
+            btnAceptar.Text = "Aceptar";
+            btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(586, 218);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(75, 23);
+            btnCancelar.TabIndex = 33;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // AgregarProducto
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(754, 253);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnAceptar);
+            Controls.Add(txtPrecio);
+            Controls.Add(lblPrecio);
+            Controls.Add(lblAgregarProducto);
+            Controls.Add(txtCosto);
+            Controls.Add(lblCostoUnitario);
+            Controls.Add(txtUnidad);
+            Controls.Add(lblUnidad);
+            Controls.Add(txtCantidad);
+            Controls.Add(lblCantidad);
+            Controls.Add(txtNombre);
+            Controls.Add(lblNombre);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox5);
+            Controls.Add(txtCodigo);
+            Controls.Add(lblCodigo);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "AgregarProducto";
+            Text = "AgregarProducto";
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblCodigo;
+        private TextBox txtCodigo;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox4;
+        private Label lblNombre;
+        private TextBox txtNombre;
+        private Label lblCantidad;
+        private TextBox txtCantidad;
+        private Label lblUnidad;
+        private TextBox txtUnidad;
+        private Label lblCostoUnitario;
+        private TextBox txtCosto;
+        private Label lblAgregarProducto;
+        private Label lblPrecio;
+        private TextBox txtPrecio;
+        private Button btnAceptar;
+        private Button btnCancelar;
     }
 }
--- sistemaCompra/AgregarProducto.cs
+++ sistemaCompra/AgregarProducto.cs
@@ -16,5 +16,25 @@ namespace sistemaCompra
         {
             InitializeComponent();
         }
+
+        /*
+         Hay que cambiar el diseño de los botones Aceptar y Cancelar
+        Así como la fuente de los labels de cada atributo de Producto
+        Y si se puede (yo creo que si), el diseño de los textboxes
+         */
+        
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
     }
 }
