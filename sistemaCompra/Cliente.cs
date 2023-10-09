a684b9777b859a6a8a760f3555818bab41df5aba
Ramón José Belandria Quintero <belandriaquinteroramonjose@gmail.com>
2023-10-09 16:19:01 -0400

    Agregadas las clases "Producto" y "Cliente"

---
+++ sistemaCompra/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemaCompra
+{
+    internal class Cliente: Auxiliares
+    {
+        private string Apellido = "";
+        private int Telefono; //Formato: 0000-0000000
+        private string Correo = "";
+        public bool TipoDeDocumento; //0 = Cédula de Identidad. 1 = RIF
+        private int NumeroDeDocumento;
+        private string TipoDeImpuesto = ""; //Exento, 16% o Retenciones
+        public string Direccion = "";
+    }
+}
---
+++ sistemaCompra/Productos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemaCompra
+{
+    internal class Productos: Auxiliares
+    {
+        private double Cantidad;
+        private string UnidadDeMedida = ""; //Kilogramos, paquetes, etc
+        private double CostoUnitario;
+        private double PrecioDeVenta;
+    }
+}
