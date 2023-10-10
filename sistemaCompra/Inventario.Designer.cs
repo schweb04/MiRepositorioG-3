5fc67d6d8bf022eed4239d158c2f0a20a70afb38
schweb04 <manelequintero@gmail.com>
2023-10-10 10:55:55 -0400

    Tabla Inventario pendiente a editar

--- sistemaCompra/Inventario.Designer.cs
+++ sistemaCompra/Inventario.Designer.cs
@@ -31,39 +31,95 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inventario));
             label1 = new Label();
             pictureBox1 = new PictureBox();
+            dtgvProductos = new DataGridView();
+            clmCodigo = new DataGridViewTextBoxColumn();
+            clmNombre = new DataGridViewTextBoxColumn();
+            clmCantidad = new DataGridViewTextBoxColumn();
+            clmUnidad = new DataGridViewTextBoxColumn();
+            clmCosto = new DataGridViewTextBoxColumn();
+            clmPrecio = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dtgvProductos).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(360, 134);
+            label1.Location = new Point(315, 100);
             label1.Name = "label1";
-            label1.Size = new Size(75, 20);
+            label1.Size = new Size(60, 15);
             label1.TabIndex = 1;
             label1.Text = "Inventario";
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(12, 389);
+            pictureBox1.Location = new Point(10, 292);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(214, 49);
+            pictureBox1.Size = new Size(187, 37);
             pictureBox1.TabIndex = 2;
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
             // 
+            // dtgvProductos
+            // 
+            dtgvProductos.AllowUserToAddRows = false;
+            dtgvProductos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtgvProductos.Columns.AddRange(new DataGridViewColumn[] { clmCodigo, clmNombre, clmCantidad, clmUnidad, clmCosto, clmPrecio });
+            dtgvProductos.Location = new Point(31, 118);
+            dtgvProductos.Name = "dtgvProductos";
+            dtgvProductos.ReadOnly = true;
+            dtgvProductos.RowTemplate.Height = 25;
+            dtgvProductos.Size = new Size(644, 150);
+            dtgvProductos.TabIndex = 3;
+            // 
+            // clmCodigo
+            // 
+            clmCodigo.HeaderText = "Código";
+            clmCodigo.Name = "clmCodigo";
+            // 
+            // clmNombre
+            // 
+            clmNombre.HeaderText = "Nombre";
+            clmNombre.Name = "clmNombre";
+            // 
+            // clmCantidad
+            // 
+            clmCantidad.HeaderText = "Cantidad";
+            clmCantidad.Name = "clmCantidad";
+            // 
+            // clmUnidad
+            // 
+            clmUnidad.HeaderText = "Ud. de Medida";
+            clmUnidad.Name = "clmUnidad";
+            // 
+            // clmCosto
+            // 
+            clmCosto.HeaderText = "Costo Unitario";
+            clmCosto.Name = "clmCosto";
+            clmCosto.ReadOnly = true;
+            // 
+            // clmPrecio
+            // 
+            clmPrecio.HeaderText = "Precio de Venta";
+            clmPrecio.Name = "clmPrecio";
+            clmPrecio.ReadOnly = true;
+            // 
             // Inventario
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
+            Controls.Add(dtgvProductos);
             Controls.Add(pictureBox1);
             Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Inventario";
             Text = "Inventario";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dtgvProductos).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -72,5 +128,12 @@
 
         private Label label1;
         private PictureBox pictureBox1;
+        private DataGridView dtgvProductos;
+        private DataGridViewTextBoxColumn clmCodigo;
+        private DataGridViewTextBoxColumn clmNombre;
+        private DataGridViewTextBoxColumn clmCantidad;
+        private DataGridViewTextBoxColumn clmUnidad;
+        private DataGridViewTextBoxColumn clmCosto;
+        private DataGridViewTextBoxColumn clmPrecio;
     }
 }
