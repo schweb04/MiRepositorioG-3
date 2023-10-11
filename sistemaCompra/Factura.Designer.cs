bfcc06f8ac26ffe5efc460f0b31dc4be71ad2537
schweb04 <manelequintero@gmail.com>
2023-10-11 12:39:30 -0400

    Subiendo cambios

--- sistemaCompra/Factura.Designer.cs
+++ sistemaCompra/Factura.Designer.cs
@@ -52,122 +52,129 @@
             TOTAL = new Label();
             label11 = new Label();
             label12 = new Label();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(48, 84);
+            label1.Location = new Point(42, 63);
             label1.Name = "label1";
-            label1.Size = new Size(55, 20);
+            label1.Size = new Size(44, 15);
             label1.TabIndex = 0;
             label1.Text = "Cedula";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(48, 117);
+            label2.Location = new Point(42, 88);
             label2.Name = "label2";
-            label2.Size = new Size(64, 20);
+            label2.Size = new Size(51, 15);
             label2.TabIndex = 1;
             label2.Text = "Nombre";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(48, 151);
+            label3.Location = new Point(42, 113);
             label3.Name = "label3";
-            label3.Size = new Size(46, 20);
+            label3.Size = new Size(36, 15);
             label3.TabIndex = 2;
             label3.Text = "Email";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(48, 188);
+            label4.Location = new Point(42, 141);
             label4.Name = "label4";
-            label4.Size = new Size(72, 20);
+            label4.Size = new Size(57, 15);
             label4.TabIndex = 3;
             label4.Text = "Direccion";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(126, 81);
+            textBox1.Location = new Point(110, 61);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
+            textBox1.Size = new Size(110, 23);
             textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(126, 114);
+            textBox2.Location = new Point(110, 86);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 27);
+            textBox2.Size = new Size(110, 23);
             textBox2.TabIndex = 5;
             textBox2.TextChanged += textBox2_TextChanged;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(126, 148);
+            textBox3.Location = new Point(110, 111);
+            textBox3.Margin = new Padding(3, 2, 3, 2);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(178, 27);
+            textBox3.Size = new Size(156, 23);
             textBox3.TabIndex = 6;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(126, 181);
+            textBox4.Location = new Point(110, 136);
+            textBox4.Margin = new Padding(3, 2, 3, 2);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(180, 27);
+            textBox4.Size = new Size(158, 23);
             textBox4.TabIndex = 7;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(51, 43);
+            label5.Location = new Point(45, 32);
             label5.Name = "label5";
-            label5.Size = new Size(55, 20);
+            label5.Size = new Size(44, 15);
             label5.TabIndex = 8;
             label5.Text = "Cliente";
             label5.Click += label5_Click;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(126, 214);
+            textBox5.Location = new Point(110, 160);
+            textBox5.Margin = new Padding(3, 2, 3, 2);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(180, 27);
+            textBox5.Size = new Size(158, 23);
             textBox5.TabIndex = 10;
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(48, 221);
+            label6.Location = new Point(42, 166);
             label6.Name = "label6";
-            label6.Size = new Size(67, 20);
+            label6.Size = new Size(52, 15);
             label6.TabIndex = 9;
             label6.Text = "Telefono";
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(103, 298);
+            label7.Location = new Point(90, 224);
             label7.Name = "label7";
-            label7.Size = new Size(69, 20);
+            label7.Size = new Size(56, 15);
             label7.TabIndex = 11;
             label7.Text = "Producto";
             // 
             // textBox6
             // 
-            textBox6.Location = new Point(112, 344);
+            textBox6.Location = new Point(98, 258);
+            textBox6.Margin = new Padding(3, 2, 3, 2);
             textBox6.Name = "textBox6";
-            textBox6.Size = new Size(125, 27);
+            textBox6.Size = new Size(110, 23);
             textBox6.TabIndex = 13;
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(48, 347);
+            label8.Location = new Point(42, 260);
             label8.Name = "label8";
-            label8.Size = new Size(58, 20);
+            label8.Size = new Size(46, 15);
             label8.TabIndex = 12;
             label8.Text = "Codigo";
             // 
@@ -177,26 +184,27 @@
             label9.BackColor = Color.FromArgb(0, 192, 0);
             label9.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             label9.ForeColor = SystemColors.ControlLightLight;
-            label9.Location = new Point(243, 344);
+            label9.Location = new Point(213, 258);
             label9.Name = "label9";
-            label9.Size = new Size(26, 28);
+            label9.Size = new Size(21, 21);
             label9.TabIndex = 14;
             label9.Text = "+";
             label9.Click += label9_Click;
             // 
             // textBox7
             // 
-            textBox7.Location = new Point(112, 382);
+            textBox7.Location = new Point(98, 286);
+            textBox7.Margin = new Padding(3, 2, 3, 2);
             textBox7.Name = "textBox7";
-            textBox7.Size = new Size(32, 27);
+            textBox7.Size = new Size(28, 23);
             textBox7.TabIndex = 16;
             // 
             // Cantidad
             // 
             Cantidad.AutoSize = true;
-            Cantidad.Location = new Point(37, 385);
+            Cantidad.Location = new Point(32, 289);
             Cantidad.Name = "Cantidad";
-            Cantidad.Size = new Size(69, 20);
+            Cantidad.Size = new Size(55, 15);
             Cantidad.TabIndex = 15;
             Cantidad.Text = "Cantidad";
             // 
@@ -204,11 +212,12 @@
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Nombre, Precio, Cantid });
-            dataGridView1.Location = new Point(327, 56);
+            dataGridView1.Location = new Point(286, 42);
+            dataGridView1.Margin = new Padding(3, 2, 3, 2);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
             dataGridView1.RowTemplate.Height = 29;
-            dataGridView1.Size = new Size(428, 61);
+            dataGridView1.Size = new Size(374, 46);
             dataGridView1.TabIndex = 17;
             dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
@@ -236,35 +245,45 @@
             // TOTAL
             // 
             TOTAL.AutoSize = true;
-            TOTAL.Location = new Point(327, 332);
+            TOTAL.Location = new Point(286, 249);
             TOTAL.Name = "TOTAL";
-            TOTAL.Size = new Size(76, 20);
+            TOTAL.Size = new Size(59, 15);
             TOTAL.TabIndex = 18;
             TOTAL.Text = "SUBTOTAL";
             // 
             // label11
             // 
             label11.AutoSize = true;
-            label11.Location = new Point(327, 368);
+            label11.Location = new Point(286, 276);
             label11.Name = "label11";
-            label11.Size = new Size(63, 20);
+            label11.Size = new Size(49, 15);
             label11.TabIndex = 19;
             label11.Text = "IVA 16%";
             // 
             // label12
             // 
             label12.AutoSize = true;
-            label12.Location = new Point(327, 402);
+            label12.Location = new Point(286, 302);
             label12.Name = "label12";
-            label12.Size = new Size(50, 20);
+            label12.Size = new Size(39, 15);
             label12.TabIndex = 20;
             label12.Text = "TOTAL";
             // 
+            // button1
+            // 
+            button1.Location = new Point(397, 287);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 23);
+            button1.TabIndex = 21;
+            button1.Text = "Generar Factura";
+            button1.UseVisualStyleBackColor = true;
+            // 
             // Factura
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(817, 444);
+            ClientSize = new Size(715, 333);
+            Controls.Add(button1);
             Controls.Add(label12);
             Controls.Add(label11);
             Controls.Add(TOTAL);
@@ -286,6 +305,7 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Factura";
             Text = "Factura";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
@@ -319,5 +339,6 @@
         private Label TOTAL;
         private Label label11;
         private Label label12;
+        private Button button1;
     }
 }
