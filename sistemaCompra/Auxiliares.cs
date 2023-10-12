59a38c7411efead6f162b3349e55270200ae5a9a
Ramón José Belandria Quintero <belandriaquinteroramonjose@gmail.com>
2023-10-12 17:02:21 -0400

    Se agregaron propiedades a las clases "Auxiliares" y "Clientee"

--- sistemaCompra/Auxiliares.cs
+++ sistemaCompra/Auxiliares.cs
@@ -8,9 +8,12 @@ namespace sistemaCompra
 {
     internal class Auxiliares
     {
-        private string Codigo;
-        private string Nombre;
-        private ushort TipoImpuesto;
+        private string codigo;
+        private string nombre;
+        private ushort tipoImpuesto;
         
+        public string Codigo {  get { return codigo; } set { codigo = value; } }
+        public string Nombre { get {  return nombre; } set {  nombre = value; } }
+        public ushort TipoImpuesto { get {  return tipoImpuesto; } set {  tipoImpuesto = value; } }
     }
 }
--- sistemaCompra/Cliente.cs
+++ sistemaCompra/Cliente.cs
@@ -8,13 +8,35 @@ namespace sistemaCompra
 {
     internal class Cliente: Auxiliares
     {
-        private string Apellido = "";
-        private int Telefono; //Formato: 0000-0000000
-        private string Correo = "";
-        public char TipoDeDocumento = 'd'; //d = Valor por defecto, V = Venezolano, E = Extranjero, J = Judicial
-        private int NumeroDeDocumento;
-        private string TipoDeImpuesto = ""; //Exento, 16% o Retenciones
-        private string Direccion = "";
-        private bool ContibuyenteEspecial;
+        private string apellido = "";
+        private int telefono; //Formato: 0000-0000000
+        private string correoElectronico = "";
+        public char tipoDeDocumento = 'd'; //d = Valor por defecto, V = Venezolano, E = Extranjero, J = Judicial
+        private int numeroDeDocumento;
+        private string tipoDeImpuesto = ""; //Exento, 16% o Retenciones
+        private string direccion = "";
+        private bool contribuyenteEspecial;
+
+        public string Apellido { get { return apellido; } set { apellido = value; } }
+        public int Telefono { get; set; }
+        public string CorreoElectronico { get {  return correoElectronico; } set {  correoElectronico = value; } }
+        public char TipoDeDocumento { get { return tipoDeDocumento; } set { tipoDeDocumento = value; } }
+        public int NumeroDeDocumento { get { return numeroDeDocumento; } set {  numeroDeDocumento = value; } }
+        public string TipoDeImpuesto { get { return tipoDeImpuesto; } set { tipoDeImpuesto= value; } }  
+        public string Direccion { get {  return direccion; } set {  direccion = value; } }
+        public bool ContribuyenteEspecial { get {  return contribuyenteEspecial; } set {  contribuyenteEspecial = value; } }
+
+        public Cliente(string _nombre ,string _apellido, int _telefono, string _correoElectronico, char _tipoDeDocumento, int _numeroDeDocumento, string _tipoDeImpuesto, string _direccion, bool _contribuyenteEspecial)
+        {
+            Nombre = _nombre;
+            Apellido = _apellido;
+            Telefono = _telefono;
+            CorreoElectronico = _correoElectronico;
+            TipoDeDocumento = _tipoDeDocumento;
+            NumeroDeDocumento = _numeroDeDocumento;
+            TipoDeImpuesto = _tipoDeImpuesto;
+            Direccion = _direccion;
+            ContribuyenteEspecial = _contribuyenteEspecial;
+        }
     }
 }
