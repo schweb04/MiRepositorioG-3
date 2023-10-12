362ad58d50d0faad77165ca0c57bc3e3df2c8822
Ramón José Belandria Quintero <belandriaquinteroramonjose@gmail.com>
2023-10-12 17:25:31 -0400

    Agregados constructores paramétricos a las clases "Auxiliares", "Cliente" y "Productos"

--- sistemaCompra/Productos.cs
+++ sistemaCompra/Productos.cs
@@ -8,11 +8,22 @@ namespace sistemaCompra
 {
     internal class Productos: Auxiliares
     {
-        private double Cantidad;
-        private string UnidadDeMedida = ""; //Kilogramos, paquetes, etc
-        private double CostoUnitario;
-        private double PrecioDeVenta;
+        private double cantidad;
+        private string unidadDeMedida = ""; //Kilogramos, paquetes, etc
+        private double costoUnitario;
+        private double precioDeVenta;   
 
-        
+        public double Cantidad {  get { return cantidad; } set { cantidad = value; } }
+        public string UnidadDeMedida { get {  return unidadDeMedida; } set {  unidadDeMedida = value; } }
+        public double CostoUnitario { get {  return costoUnitario; } set {  costoUnitario = value; } }
+        public double PrecioDeVenta {  get { return precioDeVenta; } set {  precioDeVenta = value; } }
+
+        public Productos(double _cantidad, string _unidadDeMedida, double _costoUnitario, double _precioDeVenta)
+        {
+            Cantidad = _cantidad;
+            UnidadDeMedida = _unidadDeMedida;
+            CostoUnitario = _costoUnitario;
+            PrecioDeVenta = _precioDeVenta;
+        }
     }
 }
