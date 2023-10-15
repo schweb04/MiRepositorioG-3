428f5c7320c7194a47052d696bba1f18a419e16c
Ramón José Belandria Quintero <belandriaquinteroramonjose@gmail.com>
2023-10-15 16:32:06 -0400

    Añadida la clase "Usuarios.csv"

---
+++ sistemaCompra/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemaCompra
+{
+    internal class Usuario
+    {
+        private string nombre = "";
+        private string clave = "";
+        private char tipoDeUsuario = 'N'; //N = Valor nulo | S = Superusuario | A = Administrador | C = Cajero
+
+        public string Nombre {  get { return nombre; } set {  nombre = value; } }
+        public string Clave { get {  return clave; } set {  clave = value; } }
+        public char TipoDeUsuario { get {  return tipoDeUsuario; } set {  tipoDeUsuario = value; } }
+
+        public Usuario(string _nombre, string _clave, char _tipoDeUsuario)
+        {
+            Nombre = _nombre;
+            Clave = _clave;
+            TipoDeUsuario = _tipoDeUsuario;
+            Nombre = _nombre;
+            Clave = _clave;
+            TipoDeUsuario = _tipoDeUsuario;
+        }
+    }
+}
